a2f27d8259843ea1c25155a5cf4d18e8cf65cbbc
Cubowos <erodrigez3@gmail.com>
2023-02-11 16:03:54 -0600

    atributos y metodos

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    auto a1= new auto("zzzzz","plei",1999,1223,"automatico");
+        System.out.println(a1.toString());
+
+        System.out.println(a1.avanzar(25));
+    }
+}
